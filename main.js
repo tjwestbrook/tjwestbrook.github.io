77ceec20b175f58eba8c1774b5c9d15a764e9388
tjwestbrook <taylorejwestbrook@gmail.com>
2019-07-18 01:22:57 -0400

    update UI/UX

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ function openPage(pageName, elmnt) {
     // Hide all elements with class="tabcontent" by default */
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
   
     // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "black";
+        tablinks[i].style.backgroundColor = "black";
     }
   
     // Show the specific tab content
     document.getElementById(pageName).style.display = "block";
-    }
+}
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
