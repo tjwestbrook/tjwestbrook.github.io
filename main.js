ccd55322a103779e52013038de0675ac53c3b1da
tjwestbrook <taylorejwestbrook@gmail.com>
2019-07-25 00:18:41 -0400

    functional buttons and better UI

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 function openProject(pageName, elmnt) {
     var i, tabcontent, tablinks;
+    var codeGreen = "rgb(50, 255, 200)"
     
-    // Hide all elements with class="tabcontent" by default */
+    // Hide/Show elements with class="tabcontent"
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        if (pageName == "0" || i == tabcontent.length - pageName) {
+        tabcontent[i].style.display = "block";
+        } else {
+            tabcontent[i].style.display = "none";
+        }
     }
   
-    // Remove the background color of all tablinks/buttons
+    // Change background color of tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "black";
+        tablinks[i].style.backgroundColor = "#000";
+        tablinks[i].style.color = "rgb(50, 255, 200)";
     }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = rgb(50, 255, 200);
+
+    // Set current tab and element to "open"
+    elmnt.style.backgroundColor = "rgb(50, 255, 200)";
+    elmnt.style.color = "#000";
 }
-  
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("0").click();
