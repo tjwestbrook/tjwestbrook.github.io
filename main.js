b9e52818bf9152ac38240fc74cc9f072406a4320
Taylore Westbrook <taylorejwestbrook@gmail.com>
2021-09-11 07:29:34 -0400

    portfolio upgrade

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-function openProject(pageName, elmnt) {
-    var i, tabcontent, tablinks, projects;
-    var codeGreen = "rgb(50, 255, 200)"
-    
-    // Hide/Show elements with class="tabcontent"
-    tabcontent = document.getElementsByClassName("tabcontent");
-    projects = document.getElementsByClassName("project");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-        projects[i].style.display = "none";
-        if (pageName == "0" || i == tabcontent.length - pageName) {
-            tabcontent[i].style.display = "block";
-            if (i == tabcontent.length - pageName) {
-                projects[i].style.display = "block";
-            }
-        }
+function openProject(pgNbr, elmnt) {
+    var tablink = document.getElementsByClassName("tablink");
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    var project = document.getElementsByClassName("project");
+    var pN = tabcontent.length - pgNbr;
+    var bgColor = "rgba(255, 255, 255, 0.5)";
+
+    // Hide/Show elements .tabcontent class
+    for (var i = 0; i < tabcontent.length; i++) {
+        project[i].style.display = "none"
+        if (pgNbr == 0 || i == pN) {
+            tabcontent[i].style.display = "block"
+            if (i == pN) { project[i].style.display = "block" }
+        } else { tabcontent[i].style.display = "none" }
     }
   
-    // Change background color of tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "#000";
-        tablinks[i].style.color = codeGreen;
+    for (var i = 0; i < tablink.length; i++) {
+        if (pgNbr == 0 || pgNbr != 0 && i == pN + 1) {
+            // Set current tab to valid color
+            tablink[i].style.backgroundColor = bgColor;
+        } else {
+             // Change color of null tabs
+             tablink[i].style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+        }
     }
-
-    // Set current tab and element to "open"
-    elmnt.style.backgroundColor = codeGreen;
-    elmnt.style.color = "#000";
 }
